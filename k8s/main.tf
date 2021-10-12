7d27260f6400cc6301ccb876a538e943216fcfd5
tkaburagi <t.kaburagi@me.com>
2021-10-12 14:27:03 +0900

    first commit

--- k8s/main.tf
+++ k8s/main.tf
@@ -2,6 +2,33 @@ terraform {
 
 }
 
-provider "aws" {
-	region = var.region
+provider "kubernetes" {
+	host                   = data.eks.k8s-endpoint
+	cluster_ca_certificate = data.eks.k8s-cert
+	token                  = data.eks.token
+}
+
+data "terraform_remote_state" "eks" {
+	backend = "remote"
+
+	config = {
+		organization = "tkaburagi"
+		workspaces = {
+			name = "eks-resource"
+		}
+	}
+}
+
+resource "kubernetes_namespace" "example" {
+	metadata {
+		annotations = {
+			name = "example-annotation"
+		}
+
+		labels = {
+			mylabel = "label-value"
+		}
+
+		name = "terraform-example-namespace"
+	}
 }
