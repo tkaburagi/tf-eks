fbacc378923d9884343771379688f5390bbc5145
tkaburagi <t.kaburagi@me.com>
2021-10-12 13:46:48 +0900

    first commit

--- resource/output.tf
+++ resource/output.tf
@@ -1,11 +1,3 @@
 output "k8s-endpoint" {
   value = aws_eks_cluster.my-eks-cluster.endpoint
 }
-
-output "k8s-cert" {
-  value = base64decode(aws_eks_cluster.my-eks-cluster.certificate_authority[0].data)
-}
-
-output "k8s-token" {
-  value = data.aws_eks_cluster_auth.auth.token
-}
